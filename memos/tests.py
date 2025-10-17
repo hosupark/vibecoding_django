a9cc72b8cecfe7af89cb78d13fd912abc5c3f51d
hosupark <winolege@gmail.com>
2025-10-17 16:45:59 +0900

    테스트 코드 작성: 회원가입/로그인 및 메모 CRUD 단위 테스트 추가

--- memos/tests.py
+++ memos/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Memo
 
-# Create your tests here.
+class MemoTests(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username='testuser', password='testpass1234')
+		self.client.login(username='testuser', password='testpass1234')
+
+	def test_memo_create(self):
+		response = self.client.post(reverse('memo_create'), {
+			'title': '테스트 메모',
+			'content': '메모 내용',
+		})
+		self.assertEqual(response.status_code, 302)
+		self.assertTrue(Memo.objects.filter(title='테스트 메모').exists())
+
+	def test_memo_list(self):
+		Memo.objects.create(title='메모1', content='내용', author=self.user)
+		response = self.client.get(reverse('memo_list'))
+		self.assertContains(response, '메모1')
+
+	def test_memo_update(self):
+		memo = Memo.objects.create(title='메모1', content='내용', author=self.user)
+		response = self.client.post(reverse('memo_update', args=[memo.pk]), {
+			'title': '수정된 메모',
+			'content': '수정된 내용',
+		})
+		self.assertEqual(response.status_code, 302)
+		memo.refresh_from_db()
+		self.assertEqual(memo.title, '수정된 메모')
+
+	def test_memo_delete(self):
+		memo = Memo.objects.create(title='메모1', content='내용', author=self.user)
+		response = self.client.post(reverse('memo_delete', args=[memo.pk]))
+		self.assertEqual(response.status_code, 302)
+		self.assertFalse(Memo.objects.filter(pk=memo.pk).exists())
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class UserAuthTests(TestCase):
+	def test_register(self):
+		response = self.client.post(reverse('register'), {
+			'username': 'testuser',
+			'password1': 'testpass1234',
+			'password2': 'testpass1234',
+		})
+		self.assertEqual(response.status_code, 302)
+		self.assertTrue(User.objects.filter(username='testuser').exists())
+
+	def test_login(self):
+		User.objects.create_user(username='testuser', password='testpass1234')
+		response = self.client.post(reverse('login'), {
+			'username': 'testuser',
+			'password': 'testpass1234',
+		})
+		self.assertEqual(response.status_code, 302)
