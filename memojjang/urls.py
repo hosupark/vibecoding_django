2107b9faa5bdf00ed272e335bbbd8a330b21283f
hosupark <winolege@gmail.com>
2025-10-17 16:42:34 +0900

    사용자 인증 기능 구현: 회원가입, 로그인, 로그아웃 뷰 및 템플릿 추가

--- memojjang/urls.py
+++ memojjang/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from users.views import register_view, login_view, logout_view
+
 
 urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import messages
+
+def register_view(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, '회원가입이 완료되었습니다.')
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			messages.success(request, '로그인 성공!')
+			return redirect('home')
+		else:
+			messages.error(request, '로그인 정보가 올바르지 않습니다.')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+	logout(request)
+	messages.info(request, '로그아웃 되었습니다.')
+	return redirect('home')
 
 # Create your views here.
